b2a3eaad4dd3139f20e271eed127e981f08b49f5
Aisenem02 <aisenem.ruslanova.kk@gmail.com>
2022-04-11 13:14:35 +0600

    Вывела данные из data в список product-list

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-console.log(data)
+
 
 function App() {
+  const [basket,setBasket]=React.useState([])
+  
   return (
     <div className="App">
-      <div className="product-list">
-        <button className="product" >
-          <div>Название</div>
-          <div>Цена c</div>
-        </button>
+      <div className="product-list" addToBasket={addToBasket}>
+      {data.map((el) => (
+          <button
+            className="product"
+            key={el.id}
+            onClick={() => addToBasket(el)}
+          >
+            <div>{el.title}</div>
+            <div>Цена : {el.price}</div>
+          </button>
+        ))}
       </div>
       <div className="basket">
         <div className="total">
